fc5545ac4166de8a89ec797630efac7db0172178
Telkar <telkar-rg@gmx.net>
2022-11-26 10:41:26 +0100

    added locale for faction names

---
+++ Locales.lua
@@ -0,0 +1,36 @@
+-- A very simple locale file ( GetLocale() )
+
+local AddonName, AddonTable = ...
+AddonTable["Locales"] = {}
+local L = AddonTable["Locales"]
+
+
+L["enUS"] = {
+	"Ravenholdt" = "Ravenholdt",
+	"Bloodsail Buccaneers" = "Bloodsail Buccaneers",
+	
+	"Steamwheedle Cartel" = "Steamwheedle Cartel",
+	"Booty Bay" = "Booty Bay",
+	"Everlook" = "Everlook",
+	"Gadgetzan" = "Gadgetzan",
+	"Ratchet" = "Ratchet",
+	
+	"Darkmoon Faire" = "Darkmoon Faire",
+	"Shen'dralar" = "Shen'dralar",
+}
+L["enGB"] = L["enUS"]
+
+
+L["deDe"] = {
+	"Ravenholdt" = "Rabenholdt",
+	"Bloodsail Buccaneers" = "Blutsegelbukaniere",
+	
+	"Steamwheedle Cartel" = "Dampfdruckkartell",
+	"Booty Bay" = "Beutebucht",
+	"Everlook" = "Ewige Warte",
+	"Gadgetzan" = "Gadgetzan",
+	"Ratchet" = "Ratschet",
+	
+	"Darkmoon Faire" = "Dunkelmond-Jahrmarkt",
+	"Shen'dralar" = "Shen'dralar",
+}
